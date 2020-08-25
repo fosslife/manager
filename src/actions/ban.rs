a709f578091cc8ec17fd901a8c712499b8800022
Sparkenstein <zetabytes.pp@gmail.com>
2020-08-25 23:34:04 +0530

    formatting

--- src/actions/ban.rs
+++ src/actions/ban.rs
@@ -1,15 +1,12 @@
-use tbot::contexts;
 use std::sync::Arc;
+use tbot::contexts;
 use tbot::prelude::*;
 
 pub async fn ban(ctx: Arc<contexts::Command<contexts::Text>>) {
     if let Some(message) = ctx.reply_to.clone() {
         let user_to_kick = message.from.as_ref().unwrap().id;
         let username = message.from.as_ref().unwrap().username.as_ref().unwrap();
-        ctx.kick_chat_member(user_to_kick)
-            .call()
-            .await
-            .unwrap();
+        ctx.kick_chat_member(user_to_kick).call().await.unwrap();
         ctx.send_message(&format!("Banned user {}", username))
             .call()
             .await
