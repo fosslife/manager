5cdcdaadbc3d6a10118375cf7db70e3adad55f2b
Sparkenstein <zetabytes.pp@gmail.com>
2020-08-25 23:33:50 +0530

    [feat] added configuration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+extern crate config;
+
+use config::{Config, File};
+use serde::Deserialize;
+// use std::collections::HashMap;
 use tbot::Bot;
+
 mod actions;
+#[derive(Debug, Deserialize)]
+struct BotConfig {
+    token: String,
+}
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    bot: BotConfig,
+}
 
 #[tokio::main]
 async fn main() {
-    let mut bot = Bot::from_env("MANAGER_TOKEN").event_loop();
+    let mut settings = Config::default();
+    settings.merge(File::with_name("config/default")).unwrap();
+    settings.merge(File::with_name("config/local")).unwrap();
+
+    let appconfig = settings.try_into::<AppConfig>().unwrap();
+
+    let mut bot = Bot::new(appconfig.bot.token).event_loop();
 
     bot.command("ban", actions::ban::ban);
 
