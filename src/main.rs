2ce49b2bb6bc8d8acf8aeacde36f199e029c50f2
Sparkenstein <zetabytes.pp@gmail.com>
2020-08-25 09:56:32 +0530

    [feat] mute command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tbot::{prelude::*, Bot};
+use tbot::{prelude::*, types::chat::Permissions, Bot};
 
 #[tokio::main]
 async fn main() {
@@ -8,19 +8,53 @@ async fn main() {
         if let Some(message) = ctx.reply_to.clone() {
             let user_to_kick = message.from.as_ref().unwrap().id;
             let username = message.from.as_ref().unwrap().username.as_ref().unwrap();
-            let chat_id = ctx.chat.id;
-            ctx.bot
-                .kick_chat_member(chat_id, user_to_kick)
+            // let chat_id = ctx.chat.id;
+            ctx.kick_chat_member(user_to_kick)
+                // ctx.bot
+                //     .kick_chat_member(chat_id, user_to_kick)
                 .call()
                 .await
                 .unwrap();
-            ctx.send_message(&format!("Kicked user {}", username)).call().await.unwrap();
-            
+            ctx.send_message(&format!("Kicked user {}", username))
+                .call()
+                .await
+                .unwrap();
         } else {
             // TODO: add /kick userid API
-            ctx.send_message("Please reply to a message that you want to kick!").call().await.unwrap();
+            ctx.send_message("Please reply to a message that you want to kick!")
+                .call()
+                .await
+                .unwrap();
         }
-        
+    });
+
+    bot.command("mute", |ctx| async move {
+        if let Some(message) = ctx.reply_to.clone() {
+            let user_to_mute = message.from.as_ref().unwrap().id;
+            let username = message.from.as_ref().unwrap().username.as_ref().unwrap();
+            // let chat_id = ctx.chat.id;
+            if let Err(mute_action) = ctx
+                .restrict_chat_member(user_to_mute, Permissions::default())
+                .call()
+                .await
+            {
+                ctx.send_message(&mute_action.to_string())
+                    .call()
+                    .await
+                    .unwrap();
+                dbg!(mute_action);
+            } else {
+                ctx.send_message(&format!("Muted user user {}", username))
+                    .call()
+                    .await
+                    .unwrap();
+            }
+        } else {
+            ctx.send_message("Please reply to a message that you want to kick!")
+                .call()
+                .await
+                .unwrap();
+        }
     });
 
     bot.polling().start().await.unwrap();
