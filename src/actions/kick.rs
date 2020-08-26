5aa6714439eec646801af1834d8aeab228b03efe
Sparkenstein <zetabytes.pp@gmail.com>
2020-08-26 22:53:24 +0530

    [feat] added user kicking

---
+++ src/actions/kick.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+use tbot::contexts;
+use tbot::prelude::*;
+
+pub async fn kick(ctx: Arc<contexts::Command<contexts::Text>>, _state: Arc<config::Config>) {
+    if let Some(message) = ctx.reply_to.clone() {
+        let user_to_kick = message.from.as_ref().unwrap().id;
+        let username = message.from.as_ref().unwrap().username.as_ref().unwrap();
+        ctx.unban_chat_member(user_to_kick).call().await.unwrap();
+        ctx.send_message(&format!("Kicked user {}", username))
+            .call()
+            .await
+            .unwrap();
+    } else {
+        // TODO: add /kick userid API
+        ctx.send_message("Please reply to a message that you want to kick!")
+            .call()
+            .await
+            .unwrap();
+    }
+}
--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,3 +1,4 @@
 pub mod ban;
+pub mod kick;
 pub mod mute;
 // pub mod id;
