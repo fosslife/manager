f764d19d4b4db7474c806609572af4f9c70d0935
Sparkenstein <zetabytes.pp@gmail.com>
2020-09-06 19:41:55 +0530

    [feat] rejoiend users are checked for previously promoted

--- src/handlers/new_group.rs
+++ src/handlers/new_group.rs
@@ -3,42 +3,86 @@ use rusqlite::types::Null;
 use std::sync::Arc;
 use std::time::SystemTime;
 use tbot::contexts;
+use tbot::prelude::*;
 use tokio::sync::Mutex;
-// use tbot::prelude::*;
 
 pub async fn new_memeber(ctx: Arc<contexts::NewMembers>, state: Arc<Mutex<crate::Storages>>) {
-    let db = &state.lock().await.db;
+    let state = state.lock().await;
+    // let db = &state.lock().await.db;
 
     let user = ctx.from.as_ref().unwrap();
+    info!("Got new Member {}", &user.first_name);
+
     let status = "member";
-    let joined_date = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => n.as_secs(),
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    };
-    let res = db.execute(
+
+    let joined_date = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let find_user = state.db.query_row(
         "
-        INSERT OR IGNORE INTO Users
-        VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)
-    ",
-        params![
-            user.id.0,
-            user.username,
-            user.first_name,
-            user.last_name,
-            status,
-            joined_date as u32,
-            Null
-        ],
+        SELECT userid, status FROM Users
+        WHERE userid = ?1",
+        params![user.id.0],
+        |row| {
+            let id: i64 = row.get(0).unwrap();
+            let status: String = row.get(1).unwrap();
+            Ok((id, status))
+        },
     );
 
-    match res {
-        Ok(rows) => {
-            println!("Rows written: {}", rows);
-        }
-        Err(e) => {
-            dbg!("Error occurred while saving users {}", e);
+    if let Ok(prev_user) = find_user {
+        if prev_user.1 == "admin".to_string() {
+            info!("User was admin previously, promoting in this group");
+            let res = ctx
+                .promote_chat_member(ctx.from.clone().unwrap().id)
+                .can_change_info(true)
+                .can_delete_messages(true)
+                .can_invite_users(true)
+                .can_pin_messages(true)
+                .can_promote_members(false)
+                .can_restrict_members(true)
+                .call()
+                .await;
+            if let Err(e) = res {
+                debug!("Error in promoting user {}", e);
+                ctx.send_message("Error while promoting joined user. check logs")
+                    .call()
+                    .await
+                    .unwrap();
+                return;
+            }
+            return;
+        } else {
+            info!("User was not admin previously, updating record with new information");
+            let res = state.db.execute(
+                "
+                INSERT OR REPLACE INTO Users
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)
+            ",
+                params![
+                    user.id.0,
+                    user.username,
+                    user.first_name,
+                    user.last_name,
+                    status,
+                    joined_date as u32,
+                    Null
+                ],
+            );
+
+            match res {
+                Ok(rows) => {
+                    info!("Rows written: {}", rows);
+                }
+                Err(e) => {
+                    error!("Error occurred while saving users {}", e);
+                }
+            }
         }
     }
+
+    info!("Added user to database");
     // let currnt_id = ctx.chat.id;
     // match chats.clone().into_iter().find(|e| e == &currnt_id) {
     //     Some(chat) => println!("Valid addition {:?}", chat),
